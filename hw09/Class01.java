692c145009f698d51182ddd643ccadee1443ba7c
109b13531 <91447234+109b13531@users.noreply.github.com>
2021-10-18 11:38:45 +0800

    Add files via upload

---
+++ hw09/Class01.java
@@ -0,0 +1,27 @@
+package java;
+
+class CRectangle{
+	int width;
+	int height;
+public CRectangle(int w, int h) {		//a
+	width = w; 							//a
+	height = h;							//a
+}
+void CRectangle1() { 	//b
+	width = 10; 		//b				
+	height = 8;			//b
+}
+public CRectangle() {	//c	
+	this.width = 10;	//c
+	this.height = 8;	//c
+
+}
+}
+public class Class01 {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
